55ff2e3b8b1d215917cb3bf3c8b7fc68d487974b
motiwasted <motiwasted@gmail.com>
2023-05-20 17:16:48 +0300

    First release

--- main_worker.py
+++ main_worker.py
@@ -1,17 +1,23 @@
-import time
+import os
 import telebot
 
+# Get the bot token from the Heroku environment variable
+bot_token = os.environ.get('6046559810:AAEFIo-pbGa9zgwR9X8nLlM5lluWfyiHE8I')
+
 # Create a bot instance
-bot = telebot.TeleBot('6046559810:AAEFIo-pbGa9zgwR9X8nLlM5lluWfyiHE8I')
+bot = telebot.TeleBot(bot_token)
+
+# Handle the /start command
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    response = "Hello! This is a simple Telegram bot."
+    bot.send_message(message.chat.id, response)
 
 # Handle incoming messages
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    # Check if the message text is "Деплой прошел успешно"
-    if message.text.lower() == "123":
-        response_text = "Поздравляю ты залил бота на heroku!"
-        bot.send_message(message.chat.id, response_text)
+    response = "You said: " + message.text
+    bot.send_message(message.chat.id, response)
 
 # Start the bot
-print("Bot was started \n " , time.strftime("%H:%M:%S", time.localtime()))
 bot.polling()
