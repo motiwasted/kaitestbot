bdca17405d046ed0976486691323fb8e1b8d7d86
motiwasted <82201779+motiwasted@users.noreply.github.com>
2023-06-08 21:50:23 +0300

    Update eng_schoolar.py

--- eng_schoolar.py
+++ eng_schoolar.py
@@ -1,7 +1,7 @@
 import os
 import requests
-import tensorflow as tf
 import nltk
+from bs4 import BeautifulSoup
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
@@ -13,8 +13,8 @@ model_name = 'allenai/scibert_scivocab_uncased'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = TFAutoModel.from_pretrained(model_name, from_pt=True)
 
-# Set S2ORC API endpoint
-S2ORC_API = 'https://api.semanticscholar.org/graph/v1/paper/search'
+# Set Google Scholar API endpoint
+GS_API = 'https://scholar.google.com/scholar'
 
 # Example sentence
 sentence = "The neural network is a form of artificial intelligence."
@@ -28,9 +28,11 @@ popular_bigrams = []
 for i in range(len(pos_tags)-1):
     word1, tag1 = pos_tags[i]
     word2, tag2 = pos_tags[i+1]
-    # Check if both words have allowed parts of speech
+    # Check if both words have allowed parts of speech and are not common words
     if tag1 in ['NN', 'NNS', 'JJ', 'JJR', 'JJS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'] and \
-       tag2 in ['NN', 'NNS', 'JJ', 'JJR', 'JJS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']:
+       tag2 in ['NN', 'NNS', 'JJ', 'JJR', 'JJS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'] and \
+       word1.lower() not in ['is', 'are', 'was', 'were', 'am', 'form'] and \
+       word2.lower() not in ['is', 'are', 'was', 'were', 'am', 'form']:
         bigram = f"{word1} {word2}"
         popular_bigrams.append(bigram)
 
@@ -44,37 +46,27 @@ for i, (word, tag) in enumerate(pos_tags):
         if bigram in popular_bigrams:
             selected_words.append(bigram)
             continue
-    # Check if word has an allowed part of speech
-    if tag in allowed_tags:
+    # Check if word has an allowed part of speech and is not a common word
+    if tag in allowed_tags and word.lower() not in ['is', 'are', 'was', 'were', 'am', 'form']:
         selected_words.append(word)
 
-# Get frequency count for each selected word in S2ORC dataset
-total_counts = {}
+# Get articles for selected words from Google Scholar
 for word in selected_words:
-    response = requests.get(S2ORC_API, params={'query': word, 'total': True})
+    print(f"Searching for articles on Google Scholar for '{word}':")
+    params = {'q': word, 'sort': 'date'}
+    response = requests.get(GS_API, params=params)
     if response.ok:
-        total_count = response.json().get('total', 0)
-        total_counts[word] = total_count
+        soup = BeautifulSoup(response.text, 'html.parser')
+        articles = soup.find_all('div', {'class': 'gs_ri'})
+        if articles:
+            for article in articles[:7]:
+                title = article.find('h3', {'class': 'gs_rt'})
+                link = article.find('a', href=True)
+                if title and link:
+                    print(f"Title: {title.text}")
+                    print(f"Link: {link['href']}")
+                    print()
+        else:
+            print("No articles found.")
     else:
-        print(f"Error retrieving articles for '{word}': {response.json().get('error')}")
-
-# Sort words by total count in descending order
-sorted_words = sorted(total_counts.items(), key=lambda x: x[1], reverse=True)
-
-# Print selected words and their total counts
-if len(sorted_words) > 0:
-    print(f"Total counts for selected words in S2ORC dataset:")
-    for word, count in sorted_words[:5]:
-        print(f"{word}: {count}")
-        # Search for top 10 results on Google Scholar
-        query = word.replace('"', '')
-        url = GS_URL.format(query)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        for result in soup.find_all("h3", class_="gs_rt")[:10]:
-            title = result.text
-            link = result.a["href"]
-            print(f"{title} - {link}")
-        print()
-else:
-    print("No articles found for selected words.")
+        print(f"Error retrieving articles for '{word}': {response.status_code}")
